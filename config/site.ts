a8dca1087efe05a9968fa2c5f510dd0f3457823a
ego <mrego@anakonn.com>
2024-08-30 15:04:39 +0900

    delete base

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
 	title: 'Sub Template',
 	url: 'https://docsforall.com/',
-	base: '/sub-template',
 	googleAdsSrc:
 		'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
 	githubUrl: 'https://github.com/Anakonn/docsforall-sub-template',
